d5906e500e94fd4a2dcdcb819b2732b687cab104
seefmo20203 <136596147+seefmo20203@users.noreply.github.com>
2023-06-28 14:01:05 -0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -12,13 +12,13 @@ admins = {}
 
 #------------------------ Important Stuff 🤎 -----------------------
 
-APP_ID = int(getenv("APP_ID", "18109107"))
-API_HASH = getenv("API_HASH", "e6afc7383da3a10fa16a0a9675c3e037")
-TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "5502331963:AAFxJ5vyAka0onxS-UnVFhN6-iO38-i8dUE")
-TG_BOT_USERNAME = getenv("TG_BOT_USERNAME", "Botpqyubot")
-STRING_SESSION = getenv("STRING_SESSION", "1ApWapzMBu05Zn-e0sj5yJD0hFLnYro5myrsph4QMipOY4xtGIDEQz58gT2HT4ZEG-89wPrgnoo8yhc_3YXT37_Dp-5anoOMBlArwJu_QJktbu-_mLeQPJnBGObHLdRXBI3QKPx4hmnwhx-HnV_n749QQMPgcUbTyRb8t2hBnTpjT1YhbsnUfikVknvdBCYL7Lsa1cv2BmPUAhNopP8ct5_XCFNTjD_EQML_q7rTmM8y_ZiFQ0ASP0MG7_fTr9ny6YSVNKGnOtmjAkz6TnBIimZyQaPohklfw2IPesYbcxMiX-mHp4UxWnL6wBLRruchvTAg-UGhVH67ICp2L3TBe0xRBPDDlgCU=")
+APP_ID = int(getenv("APP_ID", "23243649"))
+API_HASH = getenv("API_HASH", "9ce91443843c0cad8c2cb16455f2b65f")
+TG_BOT_TOKEN = getenv("TG_BOT_TOKEN", "5939229355:AAHq0cfdEa3qZp7QaorQlxjny4qwtBbb-QI")
+TG_BOT_USERNAME = getenv("TG_BOT_USERNAME", "smsxxnbot")
+STRING_SESSION = getenv("STRING_SESSION", "1BJWap1wBuwbkFT8xPrc0xBFMvIGrpOgvQ5_WyqwXqhy-E4z4uLvgI0TsVkxS7ij_6x_r7n1sRuvMFzUGkc8ot8mk7emAzLkEfHT6YbZPkpxlGQ8e39wh2JnQjHHMe6g7RM_LGL1Ng94ARkXZRDx3M74VmsPhesey2Z1Tl0lgc3mt-yW5abnTGe3P27pxOkqihf1MdVbEwWQSdvHnbNVuQ5m1z0pAwbrJGtBZ-uFsyY63YZ3I-p4rPvgntEO8MflB2Pbp8peGxvwx22hPv-kAJY7e48L5RHmOFsz9VFdTZNIQtxSjB5O_Q4y0D6q7PiW37JVYUpKjcNgRphjc1n_fOtSs8E-BgcQ=")
 COMMAND_HAND_LER = list(getenv("COMMAND_HAND_LER", ".").split())
 ENV = getenv("ENV", "ANYTHING")
 
 #•••••••••••••••••••••••• Mongodb Url Stuff & Loggroupid ••••••••••• 
-DATABASE_URL = getenv("DATABASE_URL", "postgres://emaqlyfh:8El0bXqfSK80NW3pXoO_DvJVDCMmuin3@snuffleupagus.db.elephantsql.com/emaqlyfh")
+DATABASE_URL = getenv("DATABASE_URL", "postgres://saclkzuf:Qvo7hD5AHcSwJpNZJvac9UKZgjOrQ1TF@snuffleupagus.db.elephantsql.com/saclkzuf")
